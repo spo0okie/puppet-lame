d5614a6cae90bc8c8c4caadef365ff2a733db50d
root <root@main.reviakin.net>
2017-07-30 15:26:46 +0500

    initial

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class lame {
+	$tmpdir='/tmp/lame_inst'
+	file {$tmpdir:
+		ensure=>directory
+	} ->
+	file {"$tmpdir/lame.tar.gz":
+		ensure	=> file,
+		source	=> 'puppet:///modules/lame/lame-3.99.5.tar.gz',
+	} ->
+	exec {'install_lame':
+		command	=> 'tar -zxvf ./lame.tar.gz && cd lame* && ./configure --prefix=/usr && make && make install',
+		cwd		=> $tmpdir,
+		unless	=> 'which lame',
+		path	=> '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+		require => Package['kernel-devel'],
+	}
+}
+
